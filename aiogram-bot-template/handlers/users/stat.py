60f775756d272fecd86f574d765d43b661f4e1b8
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-02-20 09:38:41 +0500

    Update stat.py

--- aiogram-bot-template/handlers/users/stat.py
+++ aiogram-bot-template/handlers/users/stat.py
@@ -9,10 +9,14 @@ from data.config import ADMINS
 @dp.message_handler(IsPrivate(),text="/allusers", user_id=ADMINS)
 async def add_channel(message:types.Message):
     try:
-        for message.from_user.id in ADMINS:
-             count = db.count_users()[0]
-             msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-             await bot.send_message(chat_id=ADMINS, text=msg)
+        # print(message.from_user.id)
+        # print(message)
+        # for message.from_user.id in ADMINS:
+        # print("id adminda bor")
+        count = await db.count_users()
+        # print("bazadan malumot olindi")
+        msg = f"Bazada {count} ta foydalanuvchi bor."
+        await bot.send_message(chat_id=ADMINS[0], text=msg)
+        # print("consolga userlar soni chiqarildi")
     except:
         await message.answer("Ro'yxatni ochib bo'madi")
-
