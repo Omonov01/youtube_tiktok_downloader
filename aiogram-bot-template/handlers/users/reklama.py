dd85ba40add13235b50e8addff00e2ce42327117
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-02-20 09:05:31 +0500

    Update reklama.py

--- aiogram-bot-template/handlers/users/reklama.py
+++ aiogram-bot-template/handlers/users/reklama.py
@@ -9,27 +9,6 @@ log = logging.getLogger('broadcast')
 from data.config import ADMINS
 from loader import dp, db, bot
 
-async def send_message_to_users_handler(user_id,text) :
-    try:
-        print("a")
-        await bot.an(user_id,text,message_id=bot.id)
-    except exceptions.BotBlocked:
-        logging.error(f"Target [ID:{user_id}]: blocked by user")
-    except exceptions.ChatNotFound:
-        logging.error(f"Target [ID:{user_id}]: invalid user ID")
-    except exceptions.RetryAfter as e:
-        logging.error(f"Target [ID:{user_id}]: Flood limit is exceeded. " f"Sleep {e.timeout} seconds.")
-        await asyncio.sleep(e.timeout)
-        return await bot.send_message(user_id, text)  # Recursive call
-    except exceptions.UserDeactivated:
-        logging.error(f"Target [ID:{user_id}]: user is deactivated")
-    except exceptions.TelegramAPIError:
-        logging.exception(f"Target [ID:{user_id}]: failed")
-    else:
-        logging.info(f"Target [ID:{user_id}]: success")
-        return True
-    return False
-
 @dp.message_handler(filters.IDFilter(user_id=ADMINS),
         content_types=[
             types.ContentType.PHOTO,
@@ -48,9 +27,16 @@ async def send_ad_to_all(message: types.Message):
     try:
         for user in users:
             user_id = user[3]
-            await message.copy_to(chat_id=user_id)
+            print(user_id)
+            try:
+                await message.copy_to(chat_id=user_id,reply_markup=message)
+            except:
+                pass
             count += 1
+            print(count)
             await asyncio.sleep(0.5)
-        await bot.send_message(chat_id=ADMINS, text=f"reklama {count} ta userga junatildi")
+            print("a")
+        #await message.a(chat_id=ADMINS[0], text=f"reklama {count} ta userga junatildi")
+        print("b")
     finally:
         await bot.send_message(chat_id=ADMINS, text=f"reklama {count} ta userga junatildi")
