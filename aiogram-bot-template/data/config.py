e86028399e19bb3fbef70cd09a70193b75c1f217
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-02-20 10:57:40 +0500

    Update config.py

--- aiogram-bot-template/data/config.py
+++ aiogram-bot-template/data/config.py
@@ -13,7 +13,7 @@ IP = env.str("ip")  # Xosting ip manzili
 
 BOT_LINKS = "@sehrgar_bot"
 
-DB_CONNECTION_URL = 'postgresql://postgres:okPNSpPdglRDqK5LjnrQ@containers-us-west-151.railway.app:6013/railway'
+DB_CONNECTION_URL = 'postgresql://postgres:i0ZDXc6HhB094j9tgirT@containers-us-west-188.railway.app:6956/railway'
 
 # DB_USER = "postgres"
 # DB_PASS = "okPNSpPdglRDqK5LjnrQ"
