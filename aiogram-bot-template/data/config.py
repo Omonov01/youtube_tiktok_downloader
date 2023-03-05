60bc7a11cef8fc3e5c728640b88e2d55170096c3
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-03-05 11:31:48 +0500

    Update config.py

--- aiogram-bot-template/data/config.py
+++ aiogram-bot-template/data/config.py
@@ -10,13 +10,13 @@ ADMINS = env.list("ADMINS")  # adminlar ro'yxat
 IP = env.str("ip")  # Xosting ip manzili
 BOT_LINKS = "@sehrgar_bot"
 
-#DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
-DATABASE_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
-PGDATABASE  = "railway"
-PGHOST = "containers-us-west-151.railway.app"
-PGPASSWORD = "4wnzB0ekmbUf3xaFIq58"
-PGPORT = "6735"
-PGUSER = "postgres" 
+DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
+# DATABASE_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
+# PGDATABASE  = "railway"
+# PGHOST = "containers-us-west-151.railway.app"
+# PGPASSWORD = "4wnzB0ekmbUf3xaFIq58"
+# PGPORT = "6735"
+# PGUSER = "postgres" 
 
 
 
