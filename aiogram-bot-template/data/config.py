07200105c4b6ab48decd4571e2a579d874cc9fa1
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-03-05 09:47:15 +0500

    Update config.py

--- aiogram-bot-template/data/config.py
+++ aiogram-bot-template/data/config.py
@@ -10,13 +10,13 @@ ADMINS = env.list("ADMINS")  # adminlar ro'yxat
 IP = env.str("ip")  # Xosting ip manzili
 BOT_LINKS = "@sehrgar_bot"
 
-# DB_CONNECTION_URL = "postgresql://postgres:kaWCBSSspD6j557iNJHR@containers-us-west-120.railway.app:6270/railway"
-PGHOST = env.str("containers-us-west-151.railway.app")
-PGPORT = env.str("6735")
-PGUSER = env.str("postgres")
-PGPASSWORD = env.str("4wnzB0ekmbUf3xaFIq58")
-PGDATABASE = env.str("railway")
-DATABASE_URL = env.str("postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}")
+DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
+# PGHOST = env.str("containers-us-west-151.railway.app")
+# PGPORT = env.str("6735")
+# PGUSER = env.str("postgres")
+# PGPASSWORD = env.str("4wnzB0ekmbUf3xaFIq58")
+# PGDATABASE = env.str("railway")
+# DATABASE_URL = env.str("postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}")
 
 
 
