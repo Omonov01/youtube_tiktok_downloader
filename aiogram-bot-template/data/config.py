6fa1a2ec315df3f4028cbc5b62a537d988a9ecfb
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-03-05 11:01:13 +0500

    Update config.py

--- aiogram-bot-template/data/config.py
+++ aiogram-bot-template/data/config.py
@@ -10,13 +10,17 @@ ADMINS = env.list("ADMINS")  # adminlar ro'yxat
 IP = env.str("ip")  # Xosting ip manzili
 BOT_LINKS = "@sehrgar_bot"
 
-DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
-# PGHOST = env.str("containers-us-west-151.railway.app")
-# PGPORT = env.str("6735")
-# PGUSER = env.str("postgres")
-# PGPASSWORD = env.str("4wnzB0ekmbUf3xaFIq58")
-# PGDATABASE = env.str("railway")
-# DATABASE_URL = env.str("postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}")
+#DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
+DATABASE_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
+PGDATABASE  = "railway"
+PGHOST = "containers-us-west-151.railway.app"
+PGPASSWORD = "4wnzB0ekmbUf3xaFIq58"
+PGPORT = "6735"
+PGUSER = "postgres" 
+
+
+
+
 
 
 
