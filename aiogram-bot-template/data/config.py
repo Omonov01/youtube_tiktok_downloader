a3d62481664726c925ba3a4054986b55028127d6
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-03-08 12:04:03 +0500

    Update config.py

--- aiogram-bot-template/data/config.py
+++ aiogram-bot-template/data/config.py
@@ -7,7 +7,7 @@ env.read_env()
 # # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxat
-IP = env.str("ip")  # Xosting ip manzili
+#IP = env.str("ip")  # Xosting ip manzili
 BOT_LINKS = "@sehrgar_bot"
 
 #DB_CONNECTION_URL = "postgresql://postgres:4wnzB0ekmbUf3xaFIq58@containers-us-west-151.railway.app:6735/railway"
